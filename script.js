e0e904a35b1e48d38030b6872a2409bb7f6bd667
Himanshu <himjain1999@gmail.com>
2020-10-19 21:38:07 +0530

    task complete

---
+++ script.js
@@ -0,0 +1,18 @@
+function socksAndPair(n, arr) {
+  let sorted = arr.sort( (a,b) => a-b );
+  let pairs = 0;
+for ( let i = 0; i < n - 1; i++ ) {
+    if ( sorted[i] === sorted[i + 1] ) {
+      pairs ++ ;
+      i += 1;
+    }
+  }
+return pairs;
+}
+
+
+const n = 17;
+const socks = [10, 10, 10, 10, 20, 30, 30, 30, 30, 30, 30, 30, 40, 40, 40, 40, 40]
+
+console.clear();
+console.log(`Pairs: ${socksAndPair(n, socks)}`);
